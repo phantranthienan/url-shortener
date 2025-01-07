f3ce255eb31ceafca0eef19b90f678f1826202d4
Tran Thien An PHAN <phantranthienan1405@gmail.com>
2025-01-07 14:58:51 +0100

    random

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import QueryProvider from '@/components/provider/query-provider';
-import Header from '@/components/header';
-import Footer from '@/components/footer';
+import Header from '@/components/headerx';
+import Footer from '@/components/footerx';
 
 const geistSans = Geist({
     variable: '--font-geist-sans',
